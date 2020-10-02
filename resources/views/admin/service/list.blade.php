f683740686973e49313bbb09344e7a7357f7c125
lindor <lindorhajdari@gmail.com>
2020-10-02 20:38:13 +0200

    small fix

--- resources/views/admin/service/list.blade.php
+++ resources/views/admin/service/list.blade.php
@@ -115,9 +115,9 @@
 @endsection
 
 @section('javascript')
-<script src="{{ asset('js/jquery.dataTables.js') }}"></script>
+{{-- <script src="{{ asset('js/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('js/datatables.js') }}"></script>
+<script src="{{ asset('js/datatables.js') }}"></script> --}}
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <script type="text/javascript">
