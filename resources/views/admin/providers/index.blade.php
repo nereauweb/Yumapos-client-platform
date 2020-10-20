130cb3bbb672c15041896b4d488cd568302effbd
lindor <lindorhajdari@gmail.com>
2020-10-20 18:57:35 +0200

    small fix

--- resources/views/admin/providers/index.blade.php
+++ resources/views/admin/providers/index.blade.php
@@ -1,5 +1,4 @@
 @extends('dashboard.base')
-
 @section('css')
 @endsection
 
@@ -13,79 +12,53 @@
                 <div>
                     <a href="{{ route('admin.providers.create') }}" class="btn btn-info" id="create">Add Provider</a>
                     <a href="{{ route('admin.providers.trash') }}" class="btn btn-danger" id="trash">Deleted Providers</a>
+                    @if(count($providers) > 0)
+                        <a href="{{ route('admin.referents.create') }}" class="btn btn-success" id="add-referent">Add Referent</a>
+                        <a href="{{ route('admin.referents.trash') }}" class="btn btn-danger" id="trash-referent">Deleted Referents</a>
+                    @endif
                 </div>
             </div>
             <div class="card-body">
-                <table class="table table-responsive table-bordered" style="overflow-y: auto;">
+                <table class="table table-bordered" style="overflow-y: auto;">
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>company_name</th>
-                            <th>legal_seat</th>
-                            <th>legal_seat_address</th>
-                            <th>legal_seat_zip</th>
-                            <th>legal_seat_city</th>
-                            <th>legal_seat_region</th>
-                            <th>legal_seat_country</th>
-                            <th>operative_seat</th>
-                            <th>operative_seat_address</th>
-                            <th>operative_seat_zip</th>
-                            <th>operative_seat_city</th>
-                            <th>operative_seat_region</th>
-                            <th>operative_seat_country</th>
-                            <th>vat</th>
-                            <th>tax_unique_code</th>
-                            <th>pec</th>
-                            <th>email</th>
-                            <th>phone</th>
-                            <th>website</th>
-                            <th>support_email</th>
+                            <th>Company Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Website</th>
+                            <th>Support Email</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($providers as $provider)
-                            <tr>
-                                <td>
-                                    <div class="btn-group btn-group-xs">
-                                            <button type="button" class="btn btn-table-action dropdown-toggle" data-toggle="dropdown">
-                                                {{ $provider->id }}
-                                                <i class="fa fa-ellipsis-v fa-fw" aria-hidden="true"></i>
-                                                <span class="sr-only">
+                    @foreach($providers as $provider)
+                        <tr>
+                            <td>
+                                <div class="btn-group btn-group-xs">
+                                    <button type="button" class="btn btn-table-action dropdown-toggle" data-toggle="dropdown">
+                                        {{ $provider->id }}
+                                        <i class="fa fa-ellipsis-v fa-fw" aria-hidden="true"></i>
+                                        <span class="sr-only">
                                                     Actions
                                                 </span>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a href="{{ route('admin.providers.edit', $provider->id) }}" class="dropdown-item btn-success">{{ __('coreuiforms.edit') }}</a>
-                                                <form action="{{ route('admin.providers.destroy', $provider->id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="dropdown-item btn-danger">{{ __('coreuiforms.delete') }}</button>
-                                                </form>
-                                            </div>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <a href="{{ route('admin.providers.edit', $provider->id) }}" class="dropdown-item btn-success">{{ __('coreuiforms.edit') }}</a>
+                                        <form action="{{ route('admin.providers.destroy', $provider->id) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="dropdown-item btn-danger">{{ __('coreuiforms.delete') }}</button>
+                                        </form>
                                     </div>
-                                </td>
-                                <td>{{ $provider->company_name }}</td>
-                                <td>{{ $provider->legal_seat }}</td>
-                                <td>{{ $provider->legal_seat_address }}</td>
-                                <td>{{ $provider->legal_seat_zip }}</td>
-                                <td>{{ $provider->legal_seat_city }}</td>
-                                <td>{{ $provider->legal_seat_region }}</td>
-                                <td>{{ $provider->legal_seat_country }}</td>
-                                <td>{{ $provider->operative_seat }}</td>
-                                <td>{{ $provider->operative_seat_address }}</td>
-                                <td>{{ $provider->operative_seat_zip }}</td>
-                                <td>{{ $provider->operative_seat_city }}</td>
-                                <td>{{ $provider->operative_seat_region }}</td>
-                                <td>{{ $provider->operative_seat_country }}</td>
-                                <td>{{ $provider->vat }}</td>
-                                <td>{{ $provider->tax_unique_code }}</td>
-                                <td>{{ $provider->pec }}</td>
-                                <td>{{ $provider->email }}</td>
-                                <td>{{ $provider->phone }}</td>
-                                <td>{{ $provider->website }}</td>
-                                <th>{{ $provider->support_email }}</th>
-                            </tr>
-                        @endforeach
+                                </div>
+                            </td>
+                            <td>{{ $provider->company_name }}</td>
+                            <td>{{ $provider->email }}</td>
+                            <td>{{ $provider->phone }}</td>
+                            <td>{{ $provider->website }}</td>
+                            <th>{{ $provider->support_email }}</th>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
                 {!! $providers->links() !!}
@@ -95,7 +68,6 @@
 
 
 @endsection
-
 @section('javascript')
 
 @endsection
