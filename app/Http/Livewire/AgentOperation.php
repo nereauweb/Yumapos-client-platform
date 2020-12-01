480511a7698731b253016c2074b96b830efe52a0
lhajdari <lindorhajdari@gmail.com>
2020-12-01 22:29:48 +0100

    date filter fix

--- app/Http/Livewire/AgentOperation.php
+++ app/Http/Livewire/AgentOperation.php
@@ -42,14 +42,14 @@ class AgentOperation extends Component
 			}
         }
 
-        $date_begin = ($this->from && !is_null($this->from)) ? $this->from . ' 00:00:00' : date("Y") . '-01-01 00:00:00';
-        $date_end = ($this->to && !is_null($this->to)) ? $this->to . ' 23:59:59' : date("Y") . '-12-31 23:59:59';
+        $date_begin = ($this->from && !is_null($this->from)) ? $this->from . ' 00:00:00' : date("Y-m-d") . ' 00:00:00';
+        $date_end = ($this->to && !is_null($this->to)) ? $this->to . ' 23:59:59' : date("Y-m-d") . ' 23:59:59';
 
-		$operations = ModelsAgentOperation::where('created_at','>=',$date_begin)->where('created_at','<=',$date_end)->when($this->agentSelected, function ($query) {
+		$operations = ModelsAgentOperation::where('created_at','>=', $date_begin)->where('created_at','<=',$date_end)->when($this->agentSelected, function ($query) {
 		    $query->where('user_id', $this->agentSelected);
         })->when($this->sortField, function ($query) {
             $query->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc');
-        });
+        })->orderBy('id', 'desc');
 
         if($user_id!=0){
             $operations->where('user_id',$user_id);
